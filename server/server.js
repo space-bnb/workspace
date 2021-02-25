abab3d9f5b9aa2ef3f19a3343ed416f65399224a
Benntobox <benjaminsboyle3@gmail.com>
2021-02-24 18:19:30 -0800

    Adding compression

--- server/server.js
+++ server/server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { retrieve } = require('./db/index.js')
+const { retrieve } = require('./db/index.js');
+const expressStaticGzip = require('express-static-gzip');
 
-app.use('/buildings/:workspaceId', express.static(__dirname + '/../client/dist'));
-app.use('/', express.static(__dirname + '/../client/dist'));
+app.get('*.js', (req, res, next) => {
+  console.log(`${req.url} -> ${req.url}.gz`)
+  req.url = req.url + '.gz';
+  res.set("Content-Encoding", "gzip");
+  res.set("Content-Type", "text/javascript");
+  next();
+})
+
+app.use('/', expressStaticGzip(__dirname + '/../client/dist/'));
+app.use('/buildings/:workspaceId', expressStaticGzip(__dirname + '/../client/dist/'));
 app.use(cors());
 
 app.get('/workspace-api/workspace/:id?', function(req, res) {
